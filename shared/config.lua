cbe70dd3d8fb9bfffe175687f9f65ab63264ab5d
Grandson <105886216+isgrandson@users.noreply.github.com>
2022-06-05 11:43:43 +0200

    config > globalstate

--- shared/config.lua
+++ shared/config.lua
@@ -1,35 +1,33 @@
-Config = {}
-
-Config.CarWash = {
+GlobalState.CarWash = {
 	vector3(-699.8639, -932.8102, 18.3358),
 	vector3(20.68, -1391.85, 29.32),
 	vector3(167.79, -1715.69, 29.29),
 }
 
-Config.Blip = { -- https://docs.fivem.net/docs/game-references/blips/
+GlobalState.Blip = { -- https://docs.fivem.net/docs/game-references/blips/
     ID = 100,
     Size = 1.0,
     Display = "Car Wash",
     Colour = 4
 }
 
-Config.WashPrice = 1000 -- Price for washing vehicle
-Config.WashDuration = 5 -- Duration of vehicle washing (in seconds)
+GlobalState.WashPrice = 1000 -- Price for washing vehicle
+GlobalState.WashDuration = 5 -- Duration of vehicle washing (in seconds)
 
-Config.Marker = { -- https://docs.fivem.net/docs/game-references/markers/
+GlobalState.Marker = { -- https://docs.fivem.net/docs/game-references/markers/
     Type = 36,
     ColorR = 51,
     ColorG = 153,
     ColorB = 255
 }
 
-Config.MarkerDistance = 16 -- Distance for displaying marker
-Config.TextUIDistance = 4 -- Distance for displaying ox_lib Text UI
-Config.TextUIicon = "car" -- Icon for ox_lib Text UI (font awesome icons - https://fontawesome.com/icons)
+GlobalState.MarkerDistance = 16 -- Distance for displaying marker
+GlobalState.TextUIDistance = 4 -- Distance for displaying ox_lib Text UI
+GlobalState.TextUIicon = "car" -- Icon for ox_lib Text UI (font awesome icons - https://fontawesome.com/icons)
 
 Config.OwnerCategoryEveryone = false -- If everyone can see owner category (access button, money in business) / false - only business owner can see owner category
 
-Config.Translation = {
+GlobalState.Translation = {
     -- Progress Bar
     WashingVehicle = "Washing Your Vehicle",
 
